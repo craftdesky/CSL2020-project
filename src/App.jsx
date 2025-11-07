4b4a0f0eefc5504aa67f77c5f37ae9343f7569d0
Jaenil <jaenilparekh@gmail.com>
2025-11-07 13:21:29 +0530

    implemented state to store the graph in dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Graph } from "./utils/Graph";
 import AddAirport from "./components/AddAirport";
+import GraphList from "./components/GraphList";
 
 const App = () => {
-  const handleAdd = (code) => {
-    console.log("AddAirport fired with:", code);
+  const [graph, setGraph] = useState(new Graph());
+
+  const updateGraph = (g) => {
+    setGraph(Object.assign(Object.create(Object.getPrototypeOf(g)), g));
   };
+
+  const handleAdd = (code) => {
+    const newGraph = Object.assign(Object.create(Object.getPrototypeOf(graph)), graph);
+    newGraph.addAirport(code);
+    updateGraph(newGraph);
+  };
+
+  useEffect(() => {
+    console.group("Graph Updated");
+    graph.printGraph();
+    console.groupEnd();
+  }, [graph]);
 
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>Component Test: AddAirport</h1>
       <AddAirport onAdd={handleAdd} />
+      <GraphList graph={graph} />
     </div>
   );
 };
